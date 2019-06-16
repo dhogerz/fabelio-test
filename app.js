5362e072aceda38017d345680772975a32bd60f1
fyulistian <fyulistian013@gmail.com>
2019-06-16 20:20:25 +0700

    updated

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const app = express();
 const http = require('http').Server(app);
 
+// npm cache clean --force 
+// rm -rf node_modules
+// rm -rf package-lock.json 
+// npm install
+
 // internal
 const routes = require('./routes/routes.js');
 const PORT = 8080;
--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,6 @@
 const db = require('../db/db.js');
+const axios = require('axios');
+const cheerio = require('cheerio');
 const express = require('express');
 const router = express.Router();
 
@@ -8,23 +10,46 @@ router.use(function timeLog(req, res, next) {
   next();
 });
 
+const getBreeds = () => {
+  try {
+    return axios.get('https://fabelio.com/ip/mondy-chair-3975.html')
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+const countBreeds = async () => {
+  const breeds = getBreeds()
+    .then(response => {
+      if (response.data.message) {
+        console.log(
+          `Got ${Object.entries(response.data.message).length} breeds`
+        )
+      }
+    })
+    .catch(error => {
+      console.log(error)
+    })
+}
+
 // Define the index page route
 router.get('/', (req, res) => {
-	res.status(200).send({
-		success: 'true',
-		message: 'datas retrieved successfully',
-		datas: db
-	})
-	// res.send('Main Page');
+    // https://fabelio.com/ip/mondy-chair-3975.html
+    countBreeds();
+    res.status(200).send({
+        success: 'true',
+        message: 'datas retrieved successfully',
+        datas: countBreeds()
+    })
 });
 
 // Define the home page
 router.get('/list', (req, res) => {
-	// console.log('Hi');
-    // res.render('index.ejs', {
-    //     page: "pages/list"
-    // });
-    res.send('Get the list');
+    res.status(200).send({
+        success: 'true',
+        message: 'datas retrieved successfully',
+        datas: db
+    })
 });
 
 // Define the home page
